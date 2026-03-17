027981bcfe59f2c0353de3279364587f0d569a95
prahlad-ctrl <alaparthi.prahlad@gmail.com>
2026-03-17 13:48:08 +0530

    UC1

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,6 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to BookMyStay!");
+        System.out.println("System started successfully.");
+    }
+}
