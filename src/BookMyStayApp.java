0ccb5abbad61d1dfc36c8bc0f2f0a14e34065b2d
prahlad-ctrl <alaparthi.prahlad@gmail.com>
2026-03-17 13:59:32 +0530

    UC6

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,20 +1,17 @@
 public class BookMyStayApp {
     public static void main(String[] args) {
-        System.out.println("Booking Request Queue");
-
         BookingRequestQueue bookingQueue = new BookingRequestQueue();
+        RoomInventory inventory = new RoomInventory();
+        RoomAllocationService allocationService = new RoomAllocationService();
 
         Reservation r1 = new Reservation("Abhi", "Single");
-        Reservation r2 = new Reservation("Subha", "Double");
+        Reservation r2 = new Reservation("Subha", "Single");
         Reservation r3 = new Reservation("Vanmathi", "Suite");
 
         bookingQueue.addRequest(r1);
         bookingQueue.addRequest(r2);
         bookingQueue.addRequest(r3);
 
-        while (bookingQueue.hasPendingRequests()) {
-            Reservation request = bookingQueue.processNextRequest();
-            System.out.println("Processing booking for " + request);
-        }
+        allocationService.processQueue(bookingQueue, inventory);
     }
 }
